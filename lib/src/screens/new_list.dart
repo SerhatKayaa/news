a02a4470419c4893994a027bcd6a58affa75c0ba
SerhatKayaa <serhat.kayaa3@gmail.com>
2019-06-16 12:27:08 +0300

    first commit

---
+++ lib/src/screens/new_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+
+class NewsList extends StatelessWidget {
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: buildList(),
+    );
+  }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, int index){
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot){
+            return snapshot.hasData 
+            ? Text('something')
+            : Text('else');
+          },
+        );
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 'hi',
+    );
+  }
+}
